0427b756dd3be17ea2a8c0b212e0bd7fb907d13e
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-12 12:44:32 -0500

    Set up permissions for camera and microphone usage.

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -6,23 +6,44 @@
 //  Copyright © 2019 Lambda, Inc. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		
-		// TODO: get permission
-		
-		showCamera()
-		
+		requestPermissionAndShowCamera()
 	}
+    
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        switch status {
+        case .notDetermined:
+            requestVideoPermission()
+        case .restricted:
+            fatalError("Parental controls have been enabled. Video access is denied.")
+        case .denied:
+            fatalError("Tell User to enable permission for Video/Camera")
+        case .authorized:
+            showCamera()
+        @unknown default:
+            fatalError("Apple added a new case for status that we aren't handling yet.")
+        }
+    }
+    
+    private func requestVideoPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+            guard let self = self, granted else {
+                fatalError("Tell User to enable permission for Video/Camera")
+            }
+            DispatchQueue.main.async { self.showCamera() }
+        }
+    }
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
